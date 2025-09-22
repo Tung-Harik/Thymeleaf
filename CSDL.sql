06931243f2d6c4e9972df8eb992b0ac1671c7a55
Tung-Harik <tungnm2004@gmail.com>
2025-09-22 16:15:27 +0700

    second commit

---
+++ CSDL.sql
@@ -0,0 +1,19 @@
+﻿CREATE DATABASE CategoryDB;
+GO
+USE CategoryDB;
+GO
+
+
+CREATE TABLE Categories (
+id INT IDENTITY(1,1) PRIMARY KEY,
+name NVARCHAR(100) NOT NULL,
+description NVARCHAR(500) NULL,
+created_at DATETIME2 DEFAULT SYSDATETIME(),
+updated_at DATETIME2 NULL
+);
+
+
+INSERT INTO Categories(name, description)
+VALUES (N'Skincare', N'Sản phẩm chăm sóc da'),
+(N'Makeup', N'Trang điểm'),
+(N'Haircare', N'Chăm sóc tóc');
